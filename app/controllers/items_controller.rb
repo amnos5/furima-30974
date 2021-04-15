9f5e970ec6f8ede17bc3deb4fdd902be89b46f25
amnos5 <sashimirapin@gmail.com>
2021-04-15 10:56:12 +0900

    トップ画面実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destory]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+
   def index
+
   end
 end
