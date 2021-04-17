d1c196e68850052b9a2d0909abf2f082844c8111
amnos5 <sashimirapin@gmail.com>
2021-04-18 00:19:07 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
   
 end
