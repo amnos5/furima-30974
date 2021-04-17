e05414d93e8ea1100c5a8988a104192fc3a6d9d3
amnos5 <sashimirapin@gmail.com>
2021-04-18 00:20:43 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
-  
+
+  with_options presence: true do
+    validates :user
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :postage_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_day_id, numericality: { other_than: 1 }
+  end
+
 end
