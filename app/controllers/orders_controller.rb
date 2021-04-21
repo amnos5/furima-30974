704590e5c005189829b1f6a91a912c09010d4791
amnos5 <sashimirapin@gmail.com>
2021-04-22 04:19:12 +0900

    購入のためのフォームの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,23 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-      @order = UserOrder.new
+    @item = Item.find(params[:item_id])
+    @order = UserOrder.new
+    if @item.user_id == current_user.id || @item.order!= nil
+      return redirect_to root_path
+    end
   end
   
   def create
+    @order = UserOrder.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
