16ec3e29fcfc00fc322b622c17a16dd12e5446e9
amnos5 <sashimirapin@gmail.com>
2021-04-23 20:50:29 +0900

    建物名カラム修正

--- db/migrate/20210421182328_create_deliveries.rb
+++ db/migrate/20210421182328_create_deliveries.rb
@@ -6,7 +6,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.integer :prefectures,  null: false
       t.string  :city,         null: false
       t.string  :house_number, null: false
-      t.string  :build_number, null: false
+      t.string  :build_number
       t.string  :phone_number, null: false
       t.timestamps
     end
